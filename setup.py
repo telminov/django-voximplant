67be8862fc2cdd1e99fbc8bc0152f66613b16e09
sergey@telminov.ru <sergey@telminov.ru>
2016-07-22 11:57:39 +0300

    увеличил версию

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-voximplant',
-    version='0.0.4',
+    version='0.0.5',
     description='Django application for VoxImplant integration.',
     author='Telminov Sergey',
     author_email='sergey@telminov.ru',
